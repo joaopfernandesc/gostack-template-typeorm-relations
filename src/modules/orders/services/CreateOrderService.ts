53e6e514bae079875cc8c90e6319090f0bc2a4dd
João <joaopfernandesc@gmail.com>
2020-09-20 18:45:36 -0300

    Finished

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,53 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
-  ) {}
+  ) { }
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('User does not exist');
+    }
+
+    const productsById = await this.productsRepository.findAllById(products);
+    const updateProductsQuantity: IProduct[] = [];
+
+    const orderProducts = products.map(product => {
+      const storedProduct = productsById.find(item => item.id === product.id);
+
+      if (!storedProduct) {
+        throw new AppError(`Product with id ${product.id} does not exist`);
+      } else if (storedProduct.quantity < product.quantity) {
+        throw new AppError('There is not enough quantity for this product');
+      }
+
+      updateProductsQuantity.push({
+        id: storedProduct.id,
+        quantity: storedProduct.quantity - product.quantity,
+      });
+      return {
+        product_id: storedProduct.id,
+        price: storedProduct.price,
+        quantity: product.quantity,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(updateProductsQuantity);
+    const order = await this.ordersRepository.create({
+      customer,
+      products: orderProducts,
+    });
+
+    return order;
   }
 }
 
---
+++ src/shared/infra/typeorm/migrations/1600617422090-CreateOrdersProducts.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateOrdersProducts1600617422090
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'orders_products',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          { name: 'price', type: 'decimal', precision: 8, scale: 2 },
+          { name: 'order_id', type: 'uuid' },
+          { name: 'product_id', type: 'uuid' },
+          { name: 'quantity', type: 'integer' },
+          { name: 'created_at', type: 'timestamp', default: 'now()' },
+          { name: 'updated_at', type: 'timestamp', default: 'now()' },
+        ],
+        foreignKeys: [
+          {
+            name: 'Products',
+            referencedTableName: 'products',
+            referencedColumnNames: ['id'],
+            columnNames: ['product_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'Orders',
+            referencedTableName: 'orders',
+            referencedColumnNames: ['id'],
+            columnNames: ['order_id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('orders_products');
+  }
+}
